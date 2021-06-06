96800c33c5915f767b4eda11ac63b319f5c5d8c0
leonardo.casamayor <leonardo.casamayor@gitlab.com>
2021-06-06 00:37:47 -0300

    Added variables

--- arch-install.sh
+++ arch-install.sh
@@ -1,6 +1,55 @@
 #!/bin/sh
 
 #run this script this after formating and mounting partitions
+#####Define variables#####
+user=
+userpass=
+rootpass=
+hostname="azb-ext"
+timezone="America/Argentina/Buenos_Aires"
+locale="en_US.UTF-8 UTF-8"
+lang="en_US.UTF-8"
+vconsolekeymap="us"
+x11keymap="us"
+usershell="zsh"
+pkglistURL=
+#repos
+aurhelper="yay"
+aurhelperURL="https://aur.archlinux.org/yay.git"
+dotfiles="https://gitlab.com/leonardo.casamayor/dotfiles.git"
+wallpapers="https://gitlab.com/leonardo.casamayor/wallpapers.git"
+#desktop environments (true/false)
+bspwm= true
+gnome= false
+#systemctl service to enable
+userservices=(syncthing)
+services=(NetworkManager cups tlp)
+
+#####Copy variables to files#####
+#copy config variables
+echo "#!/bin/sh" >> /mnt/config.sh
+echo "userpass=$userpass" >> /mnt/config.sh
+echo "rootpass=$rootpass" >> /mnt/config.sh
+echo "hostname=$hostname" >> /mnt/config.sh
+echo "timezone=$timezone" >> /mnt/config.sh
+echo "locale=$locale" >> /mnt/config.sh
+echo "lang=$lang" >> /mnt/config.sh
+echo "vconsolekeymap=$vconsolekeymap" >> /mnt/config.sh
+echo "x11keymap=$x11keymap" >> /mnt/config.sh
+echo "usershell=$usershell" >> /mnt/config.sh
+echo "pkglistURL=$pkglistURL" >> /mnt/config.sh
+echo "services=$services" >> /mnt/config.sh
+chmod +x /mnt/config.sh
+#copy post install variables
+echo "#!/bin/sh" >> /mnt/home/$user/postinstall.sh
+echo "dotfiles=$dotfiles" >> /mnt/home/$user/postinstall.sh
+echo "aurhelper=$aurhelper" >> /mnt/home/$user/postinstall.sh
+echo "aurhelper=$aurhelperURL" >> /mnt/home/$user/postinstall.sh
+echo "wallpapers=$wallpapers" >> /mnt/home/$user/postinstall.sh
+echo "userservices=$userservices" >> /mnt/home/$user/postinstall.sh
+echo "bspwm=$bspwm" >> /mnt/home/$user/postinstall.sh
+echo "gnome=$gnome" >> /mnt/home/$user/postinstall.sh
+chmod +x /mnt/home/$user/postinstall.sh
 
 #####Install stage#####
 timedatectl set-ntp true
@@ -17,23 +66,22 @@ cp /root/arch-install/* /mnt/arch-install
 #enter chroot environment
 arch-chroot /mnt sh -c '
 
+#source variables
+source config.sh
+#remove file
+rm config.sh
+
 #####Installation config#####
 #set timezone
-ln -sf /usr/share/zoneinfo/America/Argentina/Buenos_Aires /etc/localtime
+ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
 #sync hardware clock
 hwclock --systohc
 #set locale
 echo "#Locale enabled by arch install script" >> /etc/locale.gen
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "$locale" >> /etc/locale.gen
 locale-gen
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-#virtual console keyboard layout
-localectl set-keymap --no-convert us
-#set X keymap
-localectl set-x11-keymap us
+echo "$lang" >> /etc/locale.conf
 #network
-echo "Set your host name:"
-read host
 echo "$host" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1   localhost" >> /etc/hosts
@@ -66,21 +114,23 @@ sudo pacman -S --noconfirm arc-gtk-theme arc-icon-theme bspwm lxappearance picom
 #meslo hack source-code-pro
 
 #####Create user#####
-echo "Create a new user:"
-read user
-useradd -m -s /bin/zsh $user
-echo "Set user password:"
-passwd $user
+useradd -m -s /bin/$usershell $user
+echo "$user:$userpass" | chpasswd
 usermod -aG wheel $user
-echo "Set your root password:"
-passwd
+echo "root:$rootpass" | chpasswd
 
-#make the user the owner of the destop environment install scripts
-chmod $user:$user /arch-install/bspwm.sh
+#make the user the owner of the post install variables file
+chmod $user:$user /mnt/home/$user/postinstall.sh
 
 #####Basic config#####
+#set passwords
+
 #set ntp
 timedatectl set-ntp true
+#virtual console keymap
+localectl set-keymap --no-convert $vconsolekeymap
+#set X keymap
+localectl set-x11-keymap $x11keymap
 #pacman config
 reflector --country Brazil --counrty Chile --latest 6 --sort rate --download-timeout 60 --save /etc/pa
 pacman -Syy
@@ -92,35 +142,45 @@ echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 #set environment variable for zsh
 echo "export ZDOTDIR=\$HOME/.config/zsh" >> /etc/zsh/zshenv
 
+#####Enable systemd services#####
+systemctl enable NetworkManager
+systemctl enable cups
+systemctl enable tlp
+#systemctl enable gdm
+
 #####Run script as created user####
 su $user sh -c '
+
+#source variables
+user="$(whoami)"
+source /home/$user/postinstall.sh
+#remove file
+rm /home/$user/postinstall.sh
 
 #####Clone repos#####
 #dotfiles
 mkdir -p $HOME/.repos/dotfiles
-git clone --bare https://gitlab.com/leonardo.casamayor/dotfiles.git $HOME/.repos/dotfiles
+git clone --bare $dotfiles $HOME/.repos/dotfiles
 function gdf (){
    /usr/bin/git --git-dir=$HOME/.repos/dotfiles --work-tree=$HOME $@
 }
 gdf checkout
 if [ $? = 0 ]; then
-  echo "Checked out config.";
   else
     mkdir $HOME/config-backup
-    echo "Backing up pre-existing dot files.";
     gdf checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $HOME/config-backup/{}
 fi;
 gdf checkout
 gdf config status.showUntrackedFiles no
 #clone wallpapers repo
 mkdir $HOME/.repos/wallpapers
-git clone https://gitlab.com/leonardo.casamayor/wallpapers.git $HOME/.repos/wallpapers
+git clone $wallpapers $HOME/.repos/wallpapers
 #install yay and aur pkgs
-mkdir $HOME/.repos/yay
-git clone https://aur.archlinux.org/yay.git $HOME/.repos/yay
-cd $HOME/.repos/yay
+mkdir $HOME/.repos/$aurhelper
+git clone $aurhelperURL $HOME/.repos/$aurhelper
+cd $HOME/.repos/$aurhelper
 makepkg -si 
-yay -S zsh-theme-powerlevel10k-git polybar
+$aurhelper -S zsh-theme-powerlevel10k-git polybar
 #yay -S gotop clyrics tmsu-bin boxes ttf-vista-fonts
 #gnome extensions
 #yay gnome-shell-extension-arch-update gnome-shell-extension-pop-shell gnome-shell-extension-sound-output-device-chooser gnome-shell-extensionespresso-git 
@@ -137,14 +197,8 @@ systemctl --user enable --now syncthing
 
 '
 
-#####Enable systemd services#####
-systemctl enable NetworkManager
-systemctl enable cups
-systemctl enable tlp
-#systemctl enable gdm
-
 '
 
 #finish
-umount -R /mnt
+#umount -R /mnt
 #reboot
